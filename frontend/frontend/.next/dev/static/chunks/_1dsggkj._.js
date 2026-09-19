da442fef4af581f4373e00c35ce13edb71defef1
Natty <natty616@outlook.com>
2026-09-19 21:04:12 +0300

    fix: remove tracked .next build directory and add root gitignore

--- frontend/frontend/.next/dev/static/chunks/_1dsggkj._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/[root-of-the-server]__1x8_gvf._.css","static/chunks/node_modules_1mu25-9._.js","static/chunks/components_ui_sonner_tsx_1jmcskm._.js","static/chunks/_17stq3t._.js","static/chunks/node_modules_01l86fv._.js","static/chunks/_1r3pc-2._.js"],
-    source: "entry"
-});
--- frontend/frontend/.next/dev/static/chunks/_219uq1s._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/[root-of-the-server]__1x8_gvf._.css","static/chunks/node_modules_1mu25-9._.js","static/chunks/components_ui_sonner_tsx_1jmcskm._.js","static/chunks/_0fyekl0._.js","static/chunks/node_modules_1xfk9c5._.js"],
-    source: "entry"
-});
