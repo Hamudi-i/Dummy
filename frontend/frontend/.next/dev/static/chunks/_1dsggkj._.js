3620b0330f90dcefe332d8e8d919668cc14759ff
Natty <natty616@outlook.com>
2026-09-20 11:03:50 +0300

    remove frontend/frontend

--- frontend/frontend/.next/dev/static/chunks/_1dsggkj._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/[root-of-the-server]__1x8_gvf._.css","static/chunks/node_modules_1mu25-9._.js","static/chunks/components_ui_sonner_tsx_1jmcskm._.js","static/chunks/_1pcckhu._.js","static/chunks/node_modules_1q62j4k._.js","static/chunks/_0p-8mhz._.js"],
-    source: "entry"
-});
